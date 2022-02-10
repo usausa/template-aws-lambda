9c2b7482c55506499033a7e56e4b9a13deba0ddf
usausa <machi.pon@gmail.com>
2022-02-10 16:58:53 +0900

    Update global using

--- Template.Core/GlobalUsing.cs
+++ Template.Core/GlobalUsing.cs
@@ -3,9 +3,9 @@
 global using System.Collections;
 global using System.Collections.Generic;
 global using System.ComponentModel.DataAnnotations;
-global using System.Data.Common;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Globalization;
+global using System.Linq;
 global using System.Threading;
 global using System.Threading.Tasks;
 
@@ -13,6 +13,10 @@ global using Amazon.DynamoDBv2.DataModel;
 global using Amazon.DynamoDBv2.DocumentModel;
 
 global using Microsoft.Extensions.Logging;
+
+global using Smart;
+global using Smart.Collections.Generic;
+global using Smart.Text;
 
 global using Template.Components.DynamoDB;
 global using Template.Models;
--- Template.Lambda/GlobalUsing.cs
+++ Template.Lambda/GlobalUsing.cs
@@ -3,9 +3,9 @@
 global using System.Collections;
 global using System.Collections.Generic;
 global using System.ComponentModel.DataAnnotations;
-global using System.Data.Common;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Globalization;
+global using System.Linq;
 global using System.Threading;
 global using System.Threading.Tasks;
 
@@ -16,6 +16,10 @@ global using AmazonLambdaExtension.Annotations;
 global using AutoMapper;
 
 global using Microsoft.Extensions.Logging;
+
+global using Smart;
+global using Smart.Collections.Generic;
+global using Smart.Text;
 
 global using Template;
 global using Template.Models;
