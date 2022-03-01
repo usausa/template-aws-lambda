7eeda1d9a495f346f651c89199099d91533e675a
usausa <machi.pon@gmail.com>
2022-03-01 12:17:59 +0900

    Update

--- Template.Lambda/HttpApiFilter.cs
+++ Template.Lambda/HttpApiFilter.cs
@@ -8,6 +8,7 @@ public sealed class HttpApiFilter
 {
     public APIGatewayProxyResponse? OnFunctionExecuting(APIGatewayProxyRequest request, ILambdaContext context)
     {
+        // ReSharper disable once ConstantNullCoalescingCondition TODO remove, R# bug?
         if (request.Headers?.ContainsKey("X-Lambda-Ping") ?? false)
         {
             return new Amazon.Lambda.APIGatewayEvents.APIGatewayProxyResponse { StatusCode = 200 };
